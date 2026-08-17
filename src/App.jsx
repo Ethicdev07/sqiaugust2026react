b1a5b5107031eb7ab50b6403e613016caa8a217d
Ethicdev07 <akinadejunior76@gmail.com>
2026-08-17 10:26:13 +0100

    first commit on this class project

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import About from "./pages/About"
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+const App = ()=>{
+  return(
+
+    <>
+    
+    
+      <Router>
+
+        <Routes>
+
+          <Route path="/about" element={<About/>}/>
+
+          <Route path="/login" element={<Login/>}/>
+
+          <Route path="/signup" element={<Signup/>}/>
+
+
+
+
+
+
+        </Routes>
+
+
+
+
+
+      </Router>
+    
+    
+    </>
+
+
+
+  )
+}
+
+export default App;
